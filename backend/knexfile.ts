f7b0b64add319b1892e5acf2a1f78fc6bc5886a1
IgorFlores2003 <igorfloresbento@yahoo.com.br>
2026-04-12 12:18:53 -0300

    ajustes build

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -5,14 +5,11 @@ dotenv.config();
 
 const config: { [key: string]: Knex.Config } = {
   development: {
-    client: 'mysql2',
+    client: 'sqlite3',
     connection: {
-      host: process.env.DB_HOST || '127.0.0.1',
-      port: Number(process.env.DB_PORT) || 3306,
-      user: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'psico_db'
+      filename: './database.sqlite'
     },
+    useNullAsDefault: true,
     migrations: {
       directory: './src/database/migrations',
       extension: 'ts'
@@ -24,15 +21,11 @@ const config: { [key: string]: Knex.Config } = {
   },
 
   production: {
-    client: 'mysql2',
+    client: 'sqlite3',
     connection: {
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      ssl: { rejectUnauthorized: false }
+      filename: './database.sqlite'
     },
+    useNullAsDefault: true,
     migrations: {
       directory: './src/database/migrations',
       extension: 'ts'
